8f96414850b9940f0087e228db679ff8be21b232
Jaenai Rugengande Ihimbazwe <jaenai.rugengande-ihimbazwe@etu.unistra.fr>
2022-10-27 22:11:43 +0200

    ajout du fichier player.h

--- player.h
+++ player.h
@@ -1,3 +1,13 @@
+/**
+ * @file player.h
+ * @author Jaenai  Rugengande Ihimbazwe
+ * @brief Fichier header player
+ * @version 0.1
+ * @date 2022-10-27
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef PLAYER_HEADER
 #define PLAYER_HEADER
 
