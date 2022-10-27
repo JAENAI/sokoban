ead52908e0a89e5f9157209cb46e602fe14eb85d
Jaenai Rugengande Ihimbazwe <jaenai.rugengande-ihimbazwe@etu.unistra.fr>
2022-10-27 14:20:29 +0200

    Configuration du makefile

--- makefile.mk
+++ makefile.mk
@@ -1,5 +1,21 @@
-main : main.o grid.o
-gcc -o $@ $^ 
-%.o : %.c
-gcc -c $<
-clean: rm -f *.o
+CFLAGS = -Wall -Wextra
+LDFLAGS = -o
+CC = gcc
+
+main : main.o grid.o player.o
+	$(CC)$(CFLAGS)$(LDFLAGS) $@ $^ 
+main.o : main.c grid.h player.h
+	$(CC)$(CFLAGS) -c $<
+player.o : player.c player.h grid.h
+	$(CC)$(CFLAGS) -c $<
+grid.o : grid.c grid.h
+	$(CC)$(CFLAGS) -c $<
+clean : 
+	rm -f *.o
+doc :
+	doxygen -g
+	PROJECT_NAME ="projet-sokoban"
+	INPUT= .
+	doxygen Doxyfile
+archive :
+	tar -cf RUGENGANDE_IHIMBAZWE_Jaenai.tar.gz 'projet-sokoban'
