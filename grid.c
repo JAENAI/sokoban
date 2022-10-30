e8bb8609d964222f27de4091e862814a4987f659
Jaenai Rugengande Ihimbazwe <jaenai.rugengande-ihimbazwe@etu.unistra.fr>
2022-10-30 13:27:48 +0100

    Correction de l'allocation mémoire dans init_level()

--- grid.c
+++ grid.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "player.h"
-struct Grid init_level(const char* file_path){
+#include <string.h>
+Grid init_level(const char* file_path){
 	// ouverture du fichier en mode lecture
-	struct Grid * g=(struct Grid*)malloc(sizeof(struct Grid*));
+	Grid* g=(Grid*)malloc(sizeof(Grid));
 	FILE* file = fopen(file_path, "r");
 	if(!file){
 		fprintf(stderr, "Error %s not found", file_path);
@@ -14,34 +15,37 @@ struct Grid init_level(const char* file_path){
 	int number_column = 0; // nombre de colonne
 	int number_row = 0; /// nombre de ligne
 	int number_goals = 0;
-	// on lit la première ligne du fichier
+	// on lit la première ligne du fichier et on enregistre les tailles de notre tableau de jeu
 	fgets(line, 100, file);
 	sscanf(line, "%d %d %d", &number_column, &number_row, &number_goals);
-	g->game_grid=(enum CaseType**)malloc(sizeof(enum CaseType*)*number_row*number_column);
+	g->game_grid=malloc(number_row*sizeof(CaseType*));
+	for(int i=0;i<number_row;i++){
+		g->game_grid[i]=malloc(number_column*sizeof(CaseType));
+	}
 	g->column_number=number_column;
 	g->row_number=number_row;
 	int current_row = 0;
-	int current_goal = 0;
-	// On lit le fichier ligne par ligne jusqu'à la fin du fichier
+	int current_goal=0;
+	// On lit le fichier ligne par ligne jusqu'à la fin du fichier et on met dans notre tableau de jeu les caracteres ligne par ligne
 	while(fgets(line, 100, file) != NULL){
 		char* buffer = line;
 		int current_column = 0;
 		while(*buffer && *buffer != '\n'){
 			g->game_grid[current_row][current_column]=*buffer;
 			current_column += 1;
-			buffer += 1;
+			buffer +=1;
 			if(*buffer=='.'){
 				current_goal+=1;
 			}
 		}		
-		current_row += 1;
+		current_row+=1;
 	}
 	// fermeture du fichier
 	fclose(file);
 	return *g;
 }
 
-void display(struct Grid * g1){
+void display(Grid* g1){
 	for(int i=0; i<g1->row_number;i++){
 		for(int j=0; j<g1->column_number;j++){
 			printf("%c",g1->game_grid[i][j]);
