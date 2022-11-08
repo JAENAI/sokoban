f4cc110f61f69a0f59ab3d3c4b709faf2e6603c0
Jaenai Rugengande Ihimbazwe <jaenai.rugengande-ihimbazwe@etu.unistra.fr>
2022-11-08 22:05:52 +0100

    Ajout de fonction move_player

--- player.c
+++ player.c
@@ -2,4 +2,105 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void move_player(Grid* g,Direction d)
+void move_player(Grid* g,Direction d){
+    int x1=g->p.x;
+    int y1=g->p.y;
+    int x_g;
+    int y_g;
+    switch(d){
+        case Top:
+            if(g->game_grid[y1+1][x1]=='$'&&g->game_grid[y1+2][x1]!='#'){
+                if(g->game_grid[y1+2][x1]=='.'){
+                    x_g=x1;
+                    y_g=y1+2;
+                }
+                g->game_grid[y1+2][x1]='$';
+                g->game_grid[y1+1][x1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            else if(g->game_grid[y1+1][x1]=='.'){
+                x_g=x1;
+                y_g=y1+1;
+                g->game_grid[y1+1][x1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            else if(g->game_grid[y1+1][x1]==' '){
+                g->game_grid[y1+1][x1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            if(x1==x_g&&y1==y_g){
+                g->game_grid[x1][y1]='.';
+            }
+            break;
+        case Left:
+            if(g->game_grid[y1][x1-1]=='$'&&g->game_grid[y1][x1-2]!='#'){
+                if(g->game_grid[y1][x1-2]=='.'){
+                    x_g=x1-2;
+                    y_g=y1;
+                }
+                g->game_grid[y1][x1-2]='$';
+                g->game_grid[y1][x1-1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            else if(g->game_grid[y1][x1-1]=='.'){
+                x_g=x1;
+                y_g=y1+1;
+                g->game_grid[y1][x1-1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            else if(g->game_grid[y1][x1-1]==' '){
+                g->game_grid[y1][x1-1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            if(x1==x_g&&y1==y_g){
+                g->game_grid[y1][x1]='.';
+            }
+            break;
+        case Right:
+            if(g->game_grid[y1][x1+1]=='$'&&g->game_grid[y1][x1+2]!='#'){
+                if(g->game_grid[y1][x1+2]=='.'){
+                    x_g=x1+2;
+                    y_g=y1;
+                }
+                g->game_grid[y1][x1+2]='$';
+                g->game_grid[y1][x1+1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            else if(g->game_grid[y1][x1+1]=='.'){
+                x_g=x1+1;
+                y_g=y1;
+                g->game_grid[y1][x1+1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            else if(g->game_grid[y1][x1+1]==' '){
+                g->game_grid[y1][x1+1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            if(x1==x_g&&y1==y_g){
+                g->game_grid[y1][x1]='.';
+            }
+            break;
+        case Bottom:
+            if(g->game_grid[y1-1][x1]=='$'&&g->game_grid[y1-2][x1]!='#'){
+                if(g->game_grid[y1-2][x1]=='.'){
+                    x_g=x1;
+                    y_g=y1-2;
+                }
+                g->game_grid[y1-2][x1]='$';
+                g->game_grid[y1-1][x1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            else if(g->game_grid[y1-1][x1]=='.'){
+                x_g=x1;
+                y_g=y1-1;
+                g->game_grid[y1-1][x1]='@';
+                g->game_grid[y1][x1]=' ';
+            }
+            if(x1==x_g&&y1==y_g){
+                g->game_grid[y1][x1]='.';
+            }
+            break;
+        default:
+            break;
+     }
+}
