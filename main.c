840d7b1039081eeb8534f85b632a78b446afdad9
Jaenai Rugengande Ihimbazwe <jaenai.rugengande-ihimbazwe@etu.unistra.fr>
2022-11-08 22:06:22 +0100

    Ajout de test de move_player

--- main.c
+++ main.c
@@ -5,9 +5,27 @@ int main(void){
 	Grid g2=init_level("level1.txt");
 	display(&g2);
 	bool run = true;
+	move_player(&g2,Top);
+	display(&g2);
 	while(run){
 		char entry = fgetc(stdin);
 		switch(entry){
+			case 'h':
+				move_player(&g2,Left);
+				display(&g2);
+				break;
+			case 'j':
+				move_player(&g2,Bottom);
+				display(&g2);
+				break;
+			case 'k':
+				move_player(&g2,Top);
+				display(&g2);
+				break;
+			case 'l':
+				move_player(&g2,Right);
+				display(&g2);
+				break;
 			case 'q' :{
 				run = false;
 				break;
