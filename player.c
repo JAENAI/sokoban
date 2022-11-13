08c7933d7e780895e4fd9bd04968189ad0f201f8
Jaenai Rugengande Ihimbazwe <jaenai.rugengande-ihimbazwe@etu.unistra.fr>
2022-11-13 14:29:15 +0100

    Corretion de la sauvegarde des goals

--- player.c
+++ player.c
@@ -5,122 +5,65 @@
 void move_player(Grid* g,Direction d){
     int x1=g->p.x;
     int y1=g->p.y;
-    int x_g;
-    int y_g;
     switch(d){
         case Top:
             if(g->game_grid[y1+1][x1]=='$'&&g->game_grid[y1+2][x1]!='#'){
                 g->p.y++;
-                if(g->game_grid[y1+2][x1]=='.'){
-                    x_g=x1;
-                    y_g=y1+2;
-                }
                 g->game_grid[y1+2][x1]='$';
                 g->game_grid[y1+1][x1]='@';
                 g->game_grid[y1][x1]=' ';
             }
-            else if(g->game_grid[y1+1][x1]=='.'){
-                x_g=x1;
-                y_g=y1+1;
+            else if(g->game_grid[y1+1][x1]==' '||g->game_grid[y1+1][x1]=='.'){
                 g->p.y++;
                 g->game_grid[y1+1][x1]='@';
                 g->game_grid[y1][x1]=' ';
-            }
-            else if(g->game_grid[y1+1][x1]==' '){
-                g->p.y++;
-                g->game_grid[y1+1][x1]='@';
-                g->game_grid[y1][x1]=' ';
-            }
-            if(x1==x_g&&y1==y_g){
-                g->game_grid[y1+1][x1]='@';
-                g->game_grid[y1][x1]='.';
             }
             break;
         case Left:
             if(g->game_grid[y1][x1-1]=='$'&&g->game_grid[y1][x1-2]!='#'){
                 g->p.x--;
-                if(g->game_grid[y1][x1-2]=='.'){
-                    x_g=x1-2;
-                    y_g=y1;
-                }
                 g->game_grid[y1][x1-2]='$';
                 g->game_grid[y1][x1-1]='@';
                 g->game_grid[y1][x1]=' ';
             }
-            else if(g->game_grid[y1][x1-1]=='.'){
-                x_g=x1-1;
-                y_g=y1;
+            else if(g->game_grid[y1][x1-1]==' '||g->game_grid[y1][x1-1]=='.'){
                 g->p.x--;
                 g->game_grid[y1][x1-1]='@';
                 g->game_grid[y1][x1]=' ';
-            }
-            else if(g->game_grid[y1][x1-1]==' '){
-                g->p.x--;
-                g->game_grid[y1][x1-1]='@';
-                g->game_grid[y1][x1]=' ';
-            }
-            if(x1==x_g&&y1==y_g){
-                g->game_grid[y1][x1-1]='@';
-                g->game_grid[y1][x1]='.';
             }
             break;
         case Right:
             if(g->game_grid[y1][x1+1]=='$'&&g->game_grid[y1][x1+2]!='#'){
                 g->p.x++;
-                if(g->game_grid[y1][x1+2]=='.'){
-                    x_g=x1+2;
-                    y_g=y1;
-                }
                 g->game_grid[y1][x1+2]='$';
                 g->game_grid[y1][x1+1]='@';
                 g->game_grid[y1][x1]=' ';
             }
-            else if(g->game_grid[y1][x1+1]=='.'){
-                x_g=x1+1;
-                y_g=y1;
+            else if(g->game_grid[y1][x1+1]==' '||g->game_grid[y1][x1+1]=='.'){
                 g->p.x++;
                 g->game_grid[y1][x1+1]='@';
                 g->game_grid[y1][x1]=' ';
-            }
-            else if(g->game_grid[y1][x1+1]==' '){
-                g->p.x++;
-                g->game_grid[y1][x1+1]='@';
-                g->game_grid[y1][x1]=' ';
-            }
-            if(x1==x_g&&y1==y_g){
-                g->game_grid[y1][x1+1]='@';
-                g->game_grid[y1][x1]='.';
             }
             break;
         case Bottom:
             if(g->game_grid[y1-1][x1]=='$'&&g->game_grid[y1-2][x1]!='#'){
                 g->p.y--;
-                if(g->game_grid[y1-2][x1]=='.'){
-                    x_g=x1;
-                    y_g=y1-2;
-                }
                 g->game_grid[y1-2][x1]='$';
                 g->game_grid[y1-1][x1]='@';
                 g->game_grid[y1][x1]=' ';
             }
-            else if(g->game_grid[y1-1][x1]=='.'){
-                x_g=x1;
-                y_g=y1-1;
+            else if(g->game_grid[y1-1][x1]==' '||g->game_grid[y1-1][x1]=='.'){
                 g->p.y--;
                 g->game_grid[y1-1][x1]='@';
                 g->game_grid[y1][x1]=' ';
-            }
-            else if(g->game_grid[y1-1][x1]==' '){
-                g->p.y--;
-                g->game_grid[y1-1][x1]='@';
-                g->game_grid[y1][x1]=' ';
-            }
-            if(x1==x_g&&y1==y_g){
-                g->game_grid[y1-1][x1]='@';
-                g->game_grid[y1][x1]='.';
             }
             break;
         default:
             break;
      }
+    for(int i=0;i<g->goal_number;i++){
+        if(g->pos_goal[i][0]==y1&&g->pos_goal[i][1]==x1){
+            g->game_grid[y1][x1]='.';
+        }
+    }
 }
