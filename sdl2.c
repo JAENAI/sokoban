8c4c2f864d4fa47dc876610cf32211e7ff2af4ac
RUGENGANDE IHIMBAZWE JAENAI <jaenai.rugengande-ihimbazwe@etu.unistra.fr>
2022-12-20 10:45:34 -0500

    Ajout de la fonction display_sdl2

--- sdl2.c
+++ sdl2.c
@@ -1,5 +1,7 @@
 #include "sdl2.h"
+
 SDLContext context;
+
 void sdl_init() {
   int const width = 1280;
   int const height = 720;
@@ -19,10 +21,62 @@ void sdl_init() {
   context = (SDLContext){
       .window = window, .renderer = renderer, .width = width, .height = height};
 }
+
 void sdl_quit() {
   SDL_DestroyWindow(context.window);
   SDL_DestroyRenderer(context.renderer);
   context.window = NULL;
   context.renderer = NULL;
   SDL_Quit();
+}
+
+void display_sdl2(Grid *g1){
+  SDL_SetRenderDrawColor(context.renderer,0,0,0,255);// Couleur de fond
+  SDL_RenderClear(context.renderer);
+  for(int i=0;i<g1->row_number;i++){
+  	for(int j=0;j<g1->column_number;j++){
+  		if(g1->game_grid[i][j]=='#'){
+  			SDL_SetRenderDrawColor(context.renderer,100,100,100,255);
+  			SDL_Rect wall={
+  				.x=j*(1280/g1->column_number),
+  				.y=i*(720/g1->row_number),
+  				.w=1280/g1->column_number,
+  				.h=720/g1->row_number
+  				};
+  			SDL_RenderFillRect(context.renderer,&wall);
+  			}
+  		else if(g1->game_grid[i][j]=='$'){
+  			SDL_SetRenderDrawColor(context.renderer,255,255,255,255);
+  			SDL_Rect box={
+  				.x=j*(1280/g1->column_number),
+  				.y=i*(720/g1->row_number),
+  				.w=1280/g1->column_number,
+  				.h=720/g1->row_number
+  				};
+  			SDL_RenderFillRect(context.renderer,&box);
+  			}
+  		else if(g1->game_grid[i][j]=='.'){
+  			SDL_SetRenderDrawColor(context.renderer,255,0,0,255);
+  			SDL_Rect goal={
+  				.x=j*(1280/g1->column_number),
+  				.y=i*(720/g1->row_number),
+  				.w=(1280/2)/g1->column_number,
+  				.h=(720/2)/g1->row_number
+  				};
+  			SDL_RenderFillRect(context.renderer,&goal);
+  			}
+  		else if(g1->game_grid[i][j]=='@'){
+  			SDL_SetRenderDrawColor(context.renderer,0,0,200,255);
+  			SDL_Rect player={
+  				.x=j*(1280/g1->column_number),
+  				.y=i*(720/g1->row_number),
+  				.w=1280/g1->column_number,
+  				.h=720/g1->row_number
+  				};
+  			SDL_RenderFillRect(context.renderer,&player);
+  			}
+  		}
+  	}
+  SDL_RenderPresent(context.renderer);
+
 }
--- sdl2.h
+++ sdl2.h
@@ -1,11 +1,19 @@
 #pragma once
 #include <SDL2/SDL.h>
+#include "grid.h"
+/**
+ *@brief
+ *structure representant la fenetre de jeu
+ *@struct SDLContext
+ *
+ */
 typedef struct SDLContext {
   SDL_Window *window;
   SDL_Renderer *renderer;
   int width;
   int height;
 } SDLContext;
+
 extern SDLContext context;
 /* @brief 
  * Initialise une variable global `context` de type SDLContext
@@ -20,6 +28,15 @@ extern SDLContext context;
  */
 void sdl_init();
 /**
+ * @brief
  * nettoie la variable global context 
  */
 void sdl_quit();
+/**
+ *@brief
+ *Affiche la grille de jeu avec SDL2
+ *
+ *@param Grid *g1
+ *
+ */
+void display_sdl2(Grid *g1);
