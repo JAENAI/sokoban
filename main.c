70be3cdd35cd6511f4eb40d3774bd735a6e7955a
RUGENGANDE IHIMBAZWE JAENAI <jaenai.rugengande-ihimbazwe@etu.unistra.fr>
2022-12-21 09:04:04 -0500

    Ajout des boucles de jeu pour la version console et la version sdl2

--- main.c
+++ main.c
@@ -3,45 +3,149 @@
 #include "grid.h"
 #include "sdl2.h"
 #include <stdbool.h>
-int main(void){
-	SDLContext context;
+#include <string.h>
+
+int main(int argc,char **argv){
+	Event (*handle_event)();
+	void (*handle_display)(Grid*);
+	bool run= true;
 	Grid g2=init_level("level1.txt");
-	display(&g2);
-	bool run = true;
-	while(run){
-		char entry = fgetc(stdin);
-		switch(entry){
-			case 'h':
-				move_player(&g2,Left);
-				display(&g2);
-				break;
-			case 'j':
-				move_player(&g2,Bottom);
-				display(&g2);
-				break;
-			case 'k':
-				move_player(&g2,Top);
-				display(&g2);
-				break;
-			case 'l':
-				move_player(&g2,Right);
-				display(&g2);
-				break;
-			case 'q' :{
-				for(int i=0;i<g2.row_number;i++){
-					free(g2.game_grid[i]);
+	if(argc==1){
+		handle_event=&event;
+		handle_display=&display;
+		(*handle_display)(&g2);
+		while(run){
+			Event ev=(*handle_event)();
+			switch(ev){	
+				case Left1:
+					move_player(&g2,Left);
+					(*handle_display)(&g2);
+					break;
+				case Down:					
+					move_player(&g2,Bottom);
+					(*handle_display)(&g2);
+					break;
+				case Up:
+					move_player(&g2,Top);
+					(*handle_display)(&g2);
+					break;
+				case Right1:
+					move_player(&g2,Right);
+					(*handle_display)(&g2);
+					break;			
+				case None:
+					break;
+				case Quit :{
+					for(int i=0;i<g2.row_number;i++){
+						free(g2.game_grid[i]);
+						}
+					free(g2.game_grid);
+					for(int j=0;j<g2.goal_number;j++){
+						free(g2.pos_goal[j]);
+						}
+					free(g2.pos_goal);
+					run = false;
+					break;
+					}
 				}
-				free(g2.game_grid);
-				for(int j=0;j<g2.goal_number;j++){
-					free(g2.pos_goal[j]);
+			if(check_covered_goals(&g2)){
+				run=false;
 				}
-				free(g2.pos_goal);
-				run = false;
-				break;
+		}
+	}
+	else{
+		if(strcmp(argv[1],"--sdl2")==0){
+			handle_event=&event_sdl2;
+			handle_display=&display_sdl2;
+			sdl_init();
+			(*handle_display)(&g2);
+			while(run){
+				Event ev=(*handle_event)();
+				switch(ev){
+					case Left1:
+						move_player(&g2,Left);
+						(*handle_display)(&g2);
+						break;
+					case Down:
+						move_player(&g2,Bottom);
+						(*handle_display)(&g2);
+						break;
+					case Up:
+						move_player(&g2,Top);
+						(*handle_display)(&g2);
+						break;
+					case Right1:
+						move_player(&g2,Right);
+						(*handle_display)(&g2);
+						break;
+					case None:
+							break;
+					case Quit :{
+						for(int i=0;i<g2.row_number;i++){
+							free(g2.game_grid[i]);
+						}
+						free(g2.game_grid);
+						for(int j=0;j<g2.goal_number;j++){
+						free(g2.pos_goal[j]);
+						}
+						free(g2.pos_goal);
+						run = false;
+						break;
+					}
+				}
+				if(check_covered_goals(&g2)){
+					run=false;
+				}
+			}
+			sdl_quit();
+		}
+	
+		else if(strcmp(argv[1],"--console")==0){
+			handle_event=&event;
+			handle_display=&display;
+			(*handle_display)(&g2);
+			while(run){
+				Event ev=(*handle_event)();
+				switch(ev){	
+					case Left1:
+						move_player(&g2,Left);
+						(*handle_display)(&g2);
+						break;
+					case Down:
+						move_player(&g2,Bottom);
+						(*handle_display)(&g2);
+						break;
+					case Up:
+						move_player(&g2,Top);
+						(*handle_display)(&g2);
+						break;
+					case Right1:
+						move_player(&g2,Right);
+						(*handle_display)(&g2);
+						break;
+					case None:
+						break;
+					case Quit :{
+						for(int i=0;i<g2.row_number;i++){
+							free(g2.game_grid[i]);
+						}
+						free(g2.game_grid);
+						for(int j=0;j<g2.goal_number;j++){
+							free(g2.pos_goal[j]);
+						}
+						free(g2.pos_goal);
+						run = false;
+						break;
+					}
+				}
+				if(check_covered_goals(&g2)){
+					run=false;
+				}
 			}
 		}
-		if(check_covered_goals(&g2)){
-			run=false;
+	
+		else{
+			printf("Les arguments ne sont pas valides");
 		}
 	}
 	return 0;
